375115d2a7d4ae0683ac41764230a90a6d5e25fd
Adam Carter <adam.clayton.carter@gmail.com>
2024-01-01 16:59:29 -0500

    Choosing what to auto-wire from multiple implementations by using the @Primary annotation

--- src/main/java/config/ProjectConfiguration.java
+++ src/main/java/config/ProjectConfiguration.java
@@ -1,31 +1,9 @@
 package config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import proxies.CommentNotificationProxy;
-import proxies.EmailCommentNotificationProxy;
-import repositories.CommentRepository;
-import repositories.DBCommentRepository;
-import services.CommentService;
 
 @Configuration
+@ComponentScan(basePackages = {"proxies", "repositories", "services"})
 public class ProjectConfiguration {
-
-    @Bean
-    public CommentRepository commentRepository() {
-        return new DBCommentRepository();
-    }
-
-    @Bean
-    public CommentNotificationProxy commentNotificationProxy() {
-        return new EmailCommentNotificationProxy();
-    }
-
-    @Bean
-    public CommentService commentService(
-            CommentRepository commentRepository,
-            CommentNotificationProxy commentNotificationProxy
-    ) {
-        return new CommentService(commentRepository, commentNotificationProxy);
-    }
 }
